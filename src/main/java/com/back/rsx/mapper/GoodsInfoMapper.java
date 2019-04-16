8c895354867505d9f7f81729167636b6b845c042
raoshengxing <1667123679@qq.com>
2019-04-16 21:51:51 +0800

    删除所有的方法

--- src/main/java/com/back/rsx/mapper/GoodsInfoMapper.java
+++ src/main/java/com/back/rsx/mapper/GoodsInfoMapper.java
@@ -3,17 +3,5 @@ package com.back.rsx.mapper;
 import com.back.rsx.pojo.GoodsInfo;
 
 public interface GoodsInfoMapper {
-    int deleteByPrimaryKey(Integer goodsId);
 
-    int insert(GoodsInfo record);
-
-    int insertSelective(GoodsInfo record);
-
-    GoodsInfo selectByPrimaryKey(Integer goodsId);
-
-    int updateByPrimaryKeySelective(GoodsInfo record);
-
-    int updateByPrimaryKeyWithBLOBs(GoodsInfo record);
-
-    int updateByPrimaryKey(GoodsInfo record);
 }
--- src/main/java/com/back/rsx/mapper/GoodsTypeMapper.java
+++ src/main/java/com/back/rsx/mapper/GoodsTypeMapper.java
@@ -1,17 +1,7 @@
 package com.back.rsx.mapper;
 
-import com.itheima.mybatis.pojo.GoodsType;
+import com.back.rsx.pojo.GoodsType;
 
 public interface GoodsTypeMapper {
-    int deleteByPrimaryKey(Integer goodsTypeId);
 
-    int insert(GoodsType record);
-
-    int insertSelective(GoodsType record);
-
-    GoodsType selectByPrimaryKey(Integer goodsTypeId);
-
-    int updateByPrimaryKeySelective(GoodsType record);
-
-    int updateByPrimaryKey(GoodsType record);
 }
