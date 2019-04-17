a03f7e058d0dfb36acbb21e207feff5d64cfdc7c
yxy396224472 <396224472@qq.com>
2019-04-17 11:16:35 +0800

    yxy修改后端代码

---
+++ src/main/java/com/back/yxy/controller/LabelController.java
@@ -0,0 +1,12 @@
+package com.back.yxy.controller;
+
+import com.back.yxy.service.LabelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LabelController {
+
+
+
+}
