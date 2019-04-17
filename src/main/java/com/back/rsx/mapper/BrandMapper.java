b5826429db925e6319dfdc5ce287ed501a9d537e
raoshengxing <1667123679@qq.com>
2019-04-17 15:24:04 +0800

    修改了一些启动问题

--- src/main/java/com/back/rsx/mapper/BrandMapper.java
+++ src/main/java/com/back/rsx/mapper/BrandMapper.java
@@ -3,15 +3,5 @@ package com.back.rsx.mapper;
 import com.back.rsx.pojo.Brand;
 
 public interface BrandMapper {
-    int deleteByPrimaryKey(Integer brandId);
 
-    int insert(Brand record);
-
-    int insertSelective(Brand record);
-
-    Brand selectByPrimaryKey(Integer brandId);
-
-    int updateByPrimaryKeySelective(Brand record);
-
-    int updateByPrimaryKey(Brand record);
 }
