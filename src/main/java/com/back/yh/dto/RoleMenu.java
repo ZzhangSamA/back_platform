2f49f0e84297ba999da6ae425cb62f0e5dd22fbf
yxy396224472 <396224472@qq.com>
2019-04-25 17:11:03 +0800

    yxy提交

---
+++ src/main/java/com/back/yh/dto/RoleMenu.java
@@ -0,0 +1,23 @@
+package com.back.yh.dto;
+
+public class RoleMenu {
+
+    private int roleId;
+    private int menuId;
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(int menuId) {
+        this.menuId = menuId;
+    }
+}
---
+++ src/main/java/com/back/yh/dto/RoleUser.java
@@ -0,0 +1,23 @@
+package com.back.yh.dto;
+
+public class RoleUser {
+
+    private int roleId;
+    private int userId;
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
